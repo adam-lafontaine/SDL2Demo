f3e67ab9bf8c3c5899e13311f47eafa639216a0f
adam-lafontaine <adam.lafontiane@gmail.com>
2024-01-02 20:02:36 -0500

    move image code

--- src/app/app.cpp
+++ src/app/app.cpp
@@ -1,7 +1,12 @@
 #include "app.hpp"
-#include "../image/image.hpp"
+#include "../util/memory_buffer.hpp"
+#include "../util/stb_image/stb_include.hpp"
 
+#include <filesystem>
+#include <functional>
 #include <array>
+#include <cassert>
+#include <cstring>
 
 
 #ifndef NDEBUG
@@ -9,6 +14,371 @@
 #else
 #define printf(fmt, ...)
 #endif
+
+namespace mb = memory_buffer;
+
+
+namespace image
+{
+    template <typename T>
+    class MatrixSubView2D
+    {
+    public:
+        T*  matrix_data_;
+        u32 matrix_width;
+
+		u32 width;
+		u32 height;
+
+        union
+        {
+            Rect2Du32 range;
+
+            struct
+			{
+				u32 x_begin;
+				u32 x_end;
+				u32 y_begin;
+				u32 y_end;
+			};
+        };
+    };
+
+
+    using ImageSubView = MatrixSubView2D<Pixel>;    
+    using ImageGraySubView = MatrixSubView2D<u8>;
+    using Buffer8 = MemoryBuffer<u8>;
+    using ImageGray = Matrix2D<u8>;
+    using ImageGrayView = MatrixView2D<u8>;
+
+
+    constexpr inline Pixel to_pixel(u8 red, u8 green, u8 blue, u8 alpha)
+    {
+        Pixel p{};
+        p.red = red;
+        p.green = green;
+        p.blue = blue;
+        p.alpha = alpha;
+
+        return p;
+    }
+
+
+    constexpr inline Pixel to_pixel(u8 red, u8 green, u8 blue)
+    {
+        return to_pixel(red, green, blue, 255);
+    }
+ 
+
+
+    inline Buffer8 create_buffer8(u32 n_pixels)
+	{
+		Buffer8 buffer;
+		mb::create_buffer(buffer, n_pixels);
+		return buffer;
+	}
+}
+
+
+/* create destroy */
+
+namespace image
+{
+    bool create_image(Image& image, u32 width, u32 height)
+	{
+		assert(width);
+		assert(height);
+
+		image.data_ = (Pixel*)std::malloc(width * height * sizeof(Pixel));
+		if (!image.data_)
+		{
+			return false;
+		}
+
+		assert(image.data_ && "create_image()");
+
+        image.width = width;
+        image.height = height;
+
+		return true;
+	}
+
+    
+    void destroy_image(Image& image)
+    {
+        if (image.data_)
+		{
+			std::free((void*)image.data_);
+			image.data_ = nullptr;
+		}
+
+		image.width = 0;
+		image.height = 0;
+    }
+}
+
+
+/* row_begin */
+
+namespace image
+{
+    template <typename T>
+	static inline T* row_begin(MatrixView2D<T> const& view, u32 y)
+	{
+		return view.matrix_data_ + (u64)(y * view.width);
+	}
+
+
+    template <typename T>
+    static inline T* row_begin(MatrixSubView2D<T> const& view, u32 y)
+    {
+        return view.matrix_data_ + (u64)((view.y_begin + y) * view.matrix_width + view.x_begin);
+    }
+}
+
+
+/* make_view */
+
+namespace image
+{
+    ImageView make_view(Image const& image)
+    {
+        ImageView view{};
+
+        view.width = image.width;
+        view.height = image.height;
+        view.matrix_data_ = image.data_;
+
+        return view;
+    }
+
+
+    ImageGrayView make_view(u32 width, u32 height, Buffer8& buffer)
+    {
+        ImageGrayView view{};
+
+        view.matrix_data_ = mb::push_elements(buffer, width * height);
+        if (view.matrix_data_)
+        {
+            view.width = width;
+            view.height = height;
+        }
+
+        return view;
+    }
+}
+
+
+/* sub_view */
+
+namespace image
+{
+    template <typename T>
+    static MatrixSubView2D<T> t_sub_view(MatrixView2D<T> const& view, Rect2Du32 const& range)
+    {
+        MatrixSubView2D<T> sub_view{};
+
+        sub_view.matrix_data_ = view.matrix_data_;
+        sub_view.matrix_width = view.width;
+        sub_view.range = range;
+        sub_view.width = range.x_end - range.x_begin;
+        sub_view.height = range.y_end - range.y_begin;
+
+        return sub_view;
+    }
+
+
+    ImageSubView sub_view(ImageView const& view, Rect2Du32 const& range)
+    {
+        return t_sub_view(view, range);
+    }
+
+
+    ImageGraySubView sub_view(ImageGrayView const& view, Rect2Du32 const& range)
+    {
+        return t_sub_view(view, range);
+    }
+}
+
+
+/* fill */
+
+namespace image
+{
+    template <typename T>
+	static inline void fill_span(T* dst, T value, u32 len)
+	{
+		for (u32 i = 0; i < len; ++i)
+		{
+			dst[i] = value;
+		}
+	}
+
+
+    void fill(ImageView const& view, Pixel color)
+    {
+        fill_span(view.matrix_data_, color, view.width * view.height);
+    }
+
+
+    void fill_if(ImageGraySubView const& view, u8 gray, std::function<bool(u8)> const& pred)
+    {
+        for (u32 y = 0; y < view.height; y++)
+        {
+            auto row = row_begin(view, y);
+            for (u32 x = 0; x < view.width; x++)
+            {
+                if (pred(row[x]))
+                {
+                    row[x] = gray;
+                }
+            }
+        }
+    }
+}
+
+
+/* scale */
+
+namespace image
+{
+
+    void scale_up_to_mask(ImageView const& src, ImageGrayView const& dst, u32 scale, std::function<bool(Pixel)> const& pred)
+    {
+        assert(src.matrix_data_);
+        assert(dst.matrix_data_);
+        assert(dst.width == src.width * scale);
+        assert(dst.height == src.height * scale);
+
+        for (u32 src_y = 0; src_y < src.height; src_y++)
+        {
+            auto src_row = row_begin(src, src_y);
+            for (u32 src_x = 0; src_x < src.width; src_x++)
+            {
+                auto const s = src_row[src_x];
+
+                auto dst_y = src_y * scale;
+                for (u32 offset_y = 0; offset_y < scale; offset_y++, dst_y++)
+                {
+                    auto dst_row = row_begin(dst, dst_y);
+
+                    auto dst_x = src_x * scale;
+                    for (u32 offset_x = 0; offset_x < scale; offset_x++, dst_x++)
+                    {
+                        dst_row[dst_x] = pred(s) ? 255 : 0;
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+/* transform */
+
+namespace image
+{
+    void transform(ImageGrayView const& src, ImageSubView const& dst, std::function<Pixel(u8, Pixel)> const& func)
+    {
+        for (u32 y = 0; y < src.height; y++)
+        {
+            auto s = row_begin(src, y);
+            auto d = row_begin(dst, y);
+            for (u32 x = 0; x < src.width; x++)
+            {
+                d[x] = func(s[x], d[x]);
+            }
+        }
+    }
+
+
+    void transform_scale_up(ImageView const& src, ImageGrayView const& dst, u32 scale, std::function<u8(Pixel)> const& func)
+    {
+        assert(src.matrix_data_);
+        assert(dst.matrix_data_);
+        assert(dst.width == src.width * scale);
+        assert(dst.height == src.height * scale);
+
+        for (u32 src_y = 0; src_y < src.height; src_y++)
+        {
+            auto src_row = row_begin(src, src_y);
+            for (u32 src_x = 0; src_x < src.width; src_x++)
+            {
+                auto const s = src_row[src_x];
+
+                auto dst_y = src_y * scale;
+                for (u32 offset_y = 0; offset_y < scale; offset_y++, dst_y++)
+                {
+                    auto dst_row = row_begin(dst, dst_y);
+
+                    auto dst_x = src_x * scale;
+                    for (u32 offset_x = 0; offset_x < scale; offset_x++, dst_x++)
+                    {
+                        dst_row[dst_x] = func(s);
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+/* read */
+
+namespace image
+{
+    static bool has_extension(const char* filename, const char* ext)
+    {
+        size_t file_length = std::strlen(filename);
+        size_t ext_length = std::strlen(ext);
+
+        return !std::strcmp(&filename[file_length - ext_length], ext);
+    }
+
+
+    static bool is_valid_image_file(const char* filename)
+    {
+        return 
+            has_extension(filename, ".bmp") || 
+            has_extension(filename, ".BMP") ||
+            has_extension(filename, ".png")||
+            has_extension(filename, ".PNG");
+    }
+
+
+    bool read_image_from_file(const char* img_path_src, Image& image_dst)
+	{
+        auto is_valid_file = is_valid_image_file(img_path_src);
+        assert(is_valid_file && "invalid image file");
+
+        if (!is_valid_file)
+        {
+            return false;
+        }
+
+		int width = 0;
+		int height = 0;
+		int image_channels = 0;
+		int desired_channels = 4;
+
+		auto data = (Pixel*)stbi_load(img_path_src, &width, &height, &image_channels, desired_channels);
+
+		assert(data && "stbi_load() - no image data");
+		assert(width && "stbi_load() - no image width");
+		assert(height && "stbi_load() - no image height");
+
+		if (!data)
+		{
+			return false;
+		}
+
+		image_dst.data_ = data;
+		image_dst.width = width;
+		image_dst.height = height;
+
+		return true;
+	}
+}
 
 
 namespace img = image;
@@ -41,7 +411,7 @@ namespace
 
     bool load_keyboard_image(Image& image)
     {
-        if (!img::read_image_from_file(KEYBOARD_IMAGE_PATH, image))
+        if (!img::read_image_from_file(KEYBOARD_IMAGE_PATH.string().c_str(), image))
         {
             return false;
         }
@@ -52,7 +422,7 @@ namespace
 
     bool load_mouse_image(Image& image)
     {
-        if (!img::read_image_from_file(MOUSE_IMAGE_PATH, image))
+        if (!img::read_image_from_file(MOUSE_IMAGE_PATH.string().c_str(), image))
         {
             return false;
         }
@@ -63,7 +433,7 @@ namespace
 
     bool load_controller_image(Image& image)
     {
-        if (!img::read_image_from_file(CONTROLLER_IMAGE_PATH, image))
+        if (!img::read_image_from_file(CONTROLLER_IMAGE_PATH.string().c_str(), image))
         {
             return false;
         }
